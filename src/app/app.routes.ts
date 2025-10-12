39927bac7f4bd1e792a9efabdb3aabd492f9c3e7
Wendel Conceição Souza <wendelconceicaosouza@gmail.com>
2025-10-12 15:49:01 -0300

    Atualização das rotas conforme layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { BlankLayout } from './layouts/blank-layout/blank-layout';
 import { LoginComponent } from './auth/components/login/login.component';
 import { RegistrarComponent } from './auth/components/registrar/registrar.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegistrarComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    {
+        path: '',
+        component: BlankLayout,
+        children: [
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegistrarComponent }
+        ]
+    },
+    {
+        path: '',
+        component: MainLayout,
+        children: [
+        ]
+    },
 ];
