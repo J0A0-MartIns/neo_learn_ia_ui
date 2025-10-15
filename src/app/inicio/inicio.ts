903588eafb2d1a130fb0ab2dd4b9ab74e9e443dc
joaojesus <joaovicaps2018@gmail.com>
2025-10-15 20:08:15 -0300

    Ajustado para não permitir acesso sem login pela URL

--- src/app/inicio/inicio.ts
+++ src/app/inicio/inicio.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import {RouterModule} from "@angular/router";
-import {CommonModule} from "@angular/common";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [CommonModule, RouterModule],
   standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './inicio.html',
   styleUrl: './inicio.scss'
 })
-export class Inicio {
 
+export class Inicio implements OnInit {
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('auth_token');
+    if (!token) {
+      // sem token -> manda pro login
+      this.router.navigate(['/login']);
+    }
+  }
 }
