be0342985d1c709b6fc864e96836eb5d52480202
Wendel Conceição Souza <wendelconceicaosouza@gmail.com>
2025-11-12 19:01:16 -0300

    Criado card de projeto para a biblioteca

---
+++ src/app/components/project-card-library/project-card-library.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card-library',
+  imports: [],
+  templateUrl: './project-card-library.html',
+  styleUrl: './project-card-library.scss'
+})
+export class ProjectCardLibrary {
+
+}
