e8609da70c73146d806f153c7e567df3ec6a1eb5
EduardoUEG <escoelho@aluno.ueg.br>
2025-12-05 01:06:25 -0300

    Construção tela inicial nova

--- src/app/inicio/inicio.ts
+++ src/app/inicio/inicio.ts
@@ -1,22 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import {RouterModule, Router} from "@angular/router";
-import {CommonModule} from "@angular/common";
-
+import { RouterModule, Router } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../auth/services/auth.service';
 @Component({
-  selector: 'app-inicio',
-  imports: [CommonModule, RouterModule],
-  standalone: true,
-  templateUrl: './inicio.html',
-  styleUrl: './inicio.scss'
+    selector: 'app-inicio',
+    standalone: true,
+    imports: [
+        CommonModule,
+        RouterModule,
+        HttpClientModule
+    ],
+    templateUrl: './inicio.html',
+    styleUrl: './inicio.scss'
 })
 export class Inicio implements OnInit {
-    constructor(private router: Router) {}
+
+    public userName: string = 'Usuário';
+
+    constructor(
+        private router: Router,
+        private authService: AuthService
+    ) {}
 
     ngOnInit(): void {
         const token = localStorage.getItem('auth_token');
         if (!token) {
             this.router.navigate(['/login']);
+            return;
         }
+
+        this.carregarNomeUsuario();
     }
 
+    goToCronograma(): void {
+        this.router.navigate(['/schedule']);
+    }
+
+    carregarNomeUsuario() {
+        this.authService.getProfile().subscribe({
+            next: (data) => {
+                this.userName = data.userFirstName;
+            },
+            error: () => {
+                this.userName = 'Aluno(a)';
+            }
+        });
+    }
 }
